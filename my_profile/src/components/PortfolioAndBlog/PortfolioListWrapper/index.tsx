6c96cb9447db41e9cc6a46d5b261b247b3b8365b
miyazaki <miyazaki.dev01@gmail.com>
2025-07-12 18:12:53 +0900

    update: ポートフォリオ一覧作成

--- my_profile/src/components/PortfolioAndBlog/PortfolioListWrapper/index.tsx
+++ my_profile/src/components/PortfolioAndBlog/PortfolioListWrapper/index.tsx
@@ -3,29 +3,54 @@
 import React from "react";
 import { useScrollRestoration } from "@/hooks/useScrollRestoration";
 import { PortfoliosProps } from "@/types/PortfolioCard";
+import ListPageTitle from "@/components/PortfolioAndBlog/ListPageTitle";
 import Image from "next/image";
+import Link from "next/link";
+import {
+  portfoliosStyle,
+  portfolioStyle,
+  portfolioLinkStyle,
+  ImageDivStyle,
+  portfolioImageStyle,
+  portfolioTextStyle,
+  portfolioTitleStyle,
+  portfolioDiscriptionStyle,
+} from "./style.css";
 
 export default function PortfolioListWrapper({ portfolios }: PortfoliosProps) {
   useScrollRestoration();
 
   return (
-    <div>
-      <h1 className="text-2xl font-semibold mb-4">Portfolio</h1>
-      <div className="space-y-6">
+    <>
+      <ListPageTitle title="Portfolio" />
+      <div className={portfoliosStyle}>
         {portfolios.map((portfolio) => (
-          <div key={portfolio.id} className="space-y-2">
-            <Image
-              src={portfolio.thumbnail.url}
-              alt="Portfolio Image"
-              width={portfolio.thumbnail.width}
-              height={portfolio.thumbnail.height}
-              unoptimized
-            />
-            <h2 className="mt-2 font-bold">{portfolio.title}</h2>
-            <p>{portfolio.description}</p>
+          <div key={portfolio.id} className={portfolioStyle}>
+            <Link
+              href={`/portfolio/${portfolio.articleSlug}`}
+              className={portfolioLinkStyle}
+            >
+              <div className={ImageDivStyle}>
+                <Image
+                  src={portfolio.thumbnail.url}
+                  alt="Portfolio Image"
+                  width={portfolio.thumbnail.width}
+                  height={portfolio.thumbnail.height}
+                  unoptimized
+                  className={portfolioImageStyle}
+                />
+              </div>
+
+              <div className={portfolioTextStyle}>
+                <h2 className={portfolioTitleStyle}>{portfolio.title}</h2>
+                <p className={portfolioDiscriptionStyle}>
+                  {portfolio.description}
+                </p>
+              </div>
+            </Link>
           </div>
         ))}
       </div>
-    </div>
+    </>
   );
 }
