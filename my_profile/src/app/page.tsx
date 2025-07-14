73cffdecc3b9252beefe4bda5352903afc83eaae
miyazaki <miyazaki.dev01@gmail.com>
2025-07-14 21:37:33 +0900

    fix: トップページにのみスムーズなスクロールが適用されるように修正

--- my_profile/src/app/page.tsx
+++ my_profile/src/app/page.tsx
@@ -3,11 +3,16 @@ import { PortfolioAndBlog } from "@/components/TopPage/PortfolioAndBlog";
 import { SkillsAndQualification } from "@/components/TopPage/Skills";
 import { Career } from "@/components/TopPage/Career";
 import { Working } from "@/components/TopPage/Working";
-import { getTopContentData, getPortfolioListDataForTop, getBlogListDataForTop } from "@/libs/microcms";
+import {
+  getTopContentData,
+  getPortfolioListDataForTop,
+  getBlogListDataForTop,
+} from "@/libs/microcms";
 import { Navbar } from "@/components/TopPage/Navbar";
 import { Footer } from "@/components/TopPage/Footer";
+import SmoothScrollWrapper from "@/components/TopPage/SmoothScrollWrapper";
 
-export const dynamic = "force-static"; // 静的生成明示
+export const dynamic = "force-static"; // 静的生成
 
 export default async function Home() {
   const topData = await getTopContentData();
@@ -17,13 +22,18 @@ export default async function Home() {
   return (
     <>
       <Navbar />
-      <main className="mt-[50px]">
-        <TopContent topData={topData} />
-        <PortfolioAndBlog portfolios={portfolioCardData} blogs={BlogCardData} />
-        <SkillsAndQualification />
-        <Career />
-        <Working />
-      </main>
+      <SmoothScrollWrapper>
+        <main className="mt-[50px]">
+          <TopContent topData={topData} />
+          <PortfolioAndBlog
+            portfolios={portfolioCardData}
+            blogs={BlogCardData}
+          />
+          <SkillsAndQualification />
+          <Career />
+          <Working />
+        </main>
+      </SmoothScrollWrapper>
       <Footer />
     </>
   );
