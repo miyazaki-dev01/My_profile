c0c5ca9ea6e944b1fa2f87c9afcb2e7121458724
miyazaki <miyazaki.dev01@gmail.com>
2025-08-11 17:20:52 +0900

    update: 改行のライブラリを導入

--- my_profile/src/libs/mdxOptions.ts
+++ my_profile/src/libs/mdxOptions.ts
@@ -3,10 +3,11 @@ import rehypePrettyCode from "rehype-pretty-code";
 import type { MDXRemoteOptions } from "next-mdx-remote-client/rsc";
 import { prettyCodeOptions } from "@/libs/prettyCodeOptions";
 import type { PluggableList } from "unified";
+import remarkBreaks from "remark-breaks";
 
 export const mdxOptions: MDXRemoteOptions = {
   mdxOptions: {
-    remarkPlugins: [remarkGfm],
+    remarkPlugins: [remarkGfm, remarkBreaks],
     rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]] as PluggableList,
     format: "mdx",
   },
