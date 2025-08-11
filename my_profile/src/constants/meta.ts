33236fbc93192955ccf4f7c50a8aeb1bd418960b
miyazaki <miyazaki.dev01@gmail.com>
2025-08-11 18:42:29 +0900

    fix: トップページのmeta情報を修正

--- my_profile/src/constants/meta.ts
+++ my_profile/src/constants/meta.ts
@@ -1,7 +1,8 @@
 export const META = {
   siteTitle: "Miyazaki's profile",
   siteDescription: "宮﨑貴大のプロフィールサイトです。",
-  siteUrl: "https://my-profile-mu-gold.vercel.app",
+  siteUrl: "https://my-profile-mu-gold.vercel.app/",
+  siteImage: "/metadata/Miyazaki_profile.png",
 } as const satisfies {
   [key: string]: string;
 };
