e8e1cf967ca34ae0ce4d376945f0e7179b9acb66
miyazaki <miyazaki.dev01@gmail.com>
2025-08-11 19:01:33 +0900

    fix: ポートフォリオとブログ一覧にmeta情報を追加

--- my_profile/src/constants/meta.ts
+++ my_profile/src/constants/meta.ts
@@ -3,6 +3,12 @@ export const META = {
   siteDescription: "宮﨑貴大のプロフィールサイトです。",
   siteUrl: "https://my-profile-mu-gold.vercel.app/",
   siteImage: "/metadata/Miyazaki_profile.png",
+  portfolioTitle: "Miyazaki's profile | portfolio",
+  portfolioDescription: "ポートフォリオ一覧",
+  portfolioListUrl: "https://my-profile-mu-gold.vercel.app/portfolio/",
+  blogTitle: "Miyazaki's profile | blog",
+  blogDescription: "ブログ一覧",
+  blogListUrl: "https://my-profile-mu-gold.vercel.app/blog/",
 } as const satisfies {
   [key: string]: string;
 };
